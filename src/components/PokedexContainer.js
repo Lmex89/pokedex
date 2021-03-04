99a3c207fc519f0e4ee065e1a124d3f987f6dcb7
Luis Mex <luis@sunwise.io>
2021-03-04 17:39:33 -0600

    added-pagination-by-Lmex

--- src/components/PokedexContainer.js
+++ src/components/PokedexContainer.js
@@ -4,19 +4,22 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import PokeItem from './PokeItem';
 import NavBar from "./NavBar";
+import Pagination from "../pagination"
 
 const PokedexContainer = () => {
 
     const [pokes, setPokes] = useState([]);
     const [query, setQuery] = useState("");
+    const [currentPage, setCurrentPage] = useState(1);
+    const [poksperPage] = useState(4);
 
-    // Llamar PokeAPI
+    // Llamar PokeAPIcd
     useEffect(() => {
         if (query) {
         const promise = axios(`https://pokeapi.co/api/v2/type/${query}`);
 
         promise.then((res) => {
-            setPokes(res.data.pokemon.slice(0, 4));
+            setPokes(res.data.pokemon);
         });
         }
     }, [query]);
@@ -38,12 +41,21 @@ const PokedexContainer = () => {
         />
     ));
 
+    const indexOfLastPost = currentPage * poksperPage;
+    const indexofFirstPost = indexOfLastPost - poksperPage;
+    const currentPoks = myArrOfPokemons.slice(indexofFirstPost, indexOfLastPost);
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
     return (
         <Container fluid="true">
             <NavBar handleSearchPokemons={SearchPokemons} />
             <Container fluid="true">
                 <Row>
-                    {pokes.length > 0 && myArrOfPokemons}
+                    {pokes.length > 0 && currentPoks}
+                    <Pagination
+                        poksPerPage={poksperPage}
+                        totalPoks={myArrOfPokemons.length}
+                        paginate={paginate}/>
                 </Row>
             </Container>
         </Container>
